830e90036466f4ea9264484fba3645d26096495c
Raclax <clarasachot@gmail.com>
2024-10-14 18:45:35 +0200

    création du fichieer turtle

--- convert_csv_to_rdf.py
+++ convert_csv_to_rdf.py
@@ -7,26 +7,32 @@ def csv_to_rdf(csv_file, rdf_file):
     g = Graph()
 
     GEONAMES = Namespace("https://www.geonames.org/ontology#")
-    DBO = Namespace("https://dbpedia.org/ontology/")
     SCHEMA = Namespace("https://schema.org/")
-    EX = Namespace("https://example.org/meteorite/")
+    EX = Namespace("https://example.org/accident/")
     g.bind("ex", EX)
 
     for index, row in data.iterrows():
-        meteorite_uri = EX[str(row['id'])]
+        accident_uri = EX[str(row['Identifiant_Accident'])]
+        
+        g.add((accident_uri, SCHEMA.identifier, Literal(row['Identifiant_Accident'], datatype=XSD.string)))
+        g.add((accident_uri, SCHEMA.speedLimit, Literal(row['Vitesse_Maximale_Autorisee'], datatype=XSD.float)))
+        g.add((accident_uri, SCHEMA.day, Literal(row['jour'], datatype=XSD.integer)))
+        g.add((accident_uri, SCHEMA.month, Literal(row['mois'], datatype=XSD.string)))
+        g.add((accident_uri, SCHEMA.year, Literal(row['Annee'], datatype=XSD.gYear)))
+        g.add((accident_uri, SCHEMA.lighting, Literal(row['Luminosite'], datatype=XSD.string)))
+        g.add((accident_uri, SCHEMA.addressRegion, Literal(row['Departement'], datatype=XSD.string)))
+        g.add((accident_uri, SCHEMA.addressLocality, Literal(row['Commune'], datatype=XSD.string)))
+        g.add((accident_uri, SCHEMA.inCity, Literal(row['Agglomeration'], datatype=XSD.string)))
+        g.add((accident_uri, SCHEMA.weatherCondition, Literal(row['Connition_Atmospheriques'], datatype=XSD.string)))
+        g.add((accident_uri, SCHEMA.streetAddress, Literal(row['adresse'], datatype=XSD.string)))
 
-        g.add((meteorite_uri, SCHEMA.name, Literal(row['name'], datatype=XSD.string)))
-        g.add((meteorite_uri, DBO.recclass, Literal(row['recclass'], datatype=XSD.string)))
+        latitude = row['Latitude']
+        longitude = row['Longitude']
+        if not pd.isna(latitude) and not pd.isna(longitude):
+            g.add((accident_uri, GEONAMES.latitude, Literal(latitude, datatype=XSD.float)))
+            g.add((accident_uri, GEONAMES.longitude, Literal(longitude, datatype=XSD.float)))
 
-        mass = row['mass (g)']
-        mass = float(mass) if not pd.isna(mass) else 0.0
-
-        g.add((meteorite_uri, SCHEMA.weight, Literal(mass, datatype=XSD.float)))
-        g.add((meteorite_uri, SCHEMA.Event, Literal(row['fall'], datatype=XSD.string)))
-        g.add((meteorite_uri, SCHEMA.year, Literal(row['year'], datatype=XSD.gYear)))
-        g.add((meteorite_uri, GEONAMES.latitude, Literal(row['reclat'], datatype=XSD.float)))
-        g.add((meteorite_uri, GEONAMES.longitude, Literal(row['reclong'], datatype=XSD.float)))
 
     g.serialize(destination=rdf_file, format="turtle")
 
-csv_to_rdf('../data/Meteorite_Landings_20240924.csv', '../output/meteorite_landings.ttl')
+csv_to_rdf('accidents.csv', 'accidents.ttl')
